ca244d21e211f881fdda97019723ffa52579380e
TonuSH <142290574+TonuSH@users.noreply.github.com>
2023-11-18 07:55:06 -0800

    Delete js/app.js

--- js/app.js
+++
@@ -1,7 +0,0 @@
-document.querySelector("input").addEventListener("input",range);
-
-function range(){
-    let rangeVal= document.querySelector("input").value;
-
-    document.querySelector('p').innerText=rangeVal;
-}
